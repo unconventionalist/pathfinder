cdae00c36d3e5286a86d10253914b9e15e171f01
Pranta Alam <prantalam@gmail.com>
2020-05-22 20:06:33 -0400

    Updates

---
+++ precache-manifest.f4f7fa849f38f4c5b667613d8db4fc1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede7b62cee2dc246d27524f5b51fa815",
+    "url": "/pathfinder/index.html"
+  },
+  {
+    "revision": "00ce25cc7a40150e7b30",
+    "url": "/pathfinder/static/css/main.c21fe03d.chunk.css"
+  },
+  {
+    "revision": "7d5b0861c58a2d0f9c65",
+    "url": "/pathfinder/static/js/2.262cb136.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/pathfinder/static/js/2.262cb136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00ce25cc7a40150e7b30",
+    "url": "/pathfinder/static/js/main.15a3d437.chunk.js"
+  },
+  {
+    "revision": "9ccc5e35736b14838482",
+    "url": "/pathfinder/static/js/runtime-main.8f225125.js"
+  },
+  {
+    "revision": "11678bdad691d88edc2585199bb3af0d",
+    "url": "/pathfinder/static/media/chevron-right.11678bda.svg"
+  },
+  {
+    "revision": "76212db24303560da870a01930840c26",
+    "url": "/pathfinder/static/media/target.76212db2.svg"
+  }
+]);
